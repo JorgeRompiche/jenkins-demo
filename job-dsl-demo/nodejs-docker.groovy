3009b44be9ad6828f093047de45aae113ef47859
Jorgearh <jorgerompiche@gmail.com>
2023-06-19 16:06:16 -0600

    nodejs-docker job dsl demo

---
+++ job-dsl-demo/nodejs-docker.groovy
@@ -0,0 +1,25 @@
+jog('nodejs-docker-example'){
+    scm {
+        git('https://github.com/JorgeRompiche/nodejs-app-demo.git') { node -> 
+            node / gitConfigName('Jorge Rompiche')
+            node / gitConfigEmail('jarh@mail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('jorgearh/nodejs-app-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
